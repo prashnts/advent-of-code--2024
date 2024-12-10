da678e802245eae6a8a68013ec688e00ba6cdec1
Prashant Sinha <prashant@noop.pw>
2024-12-10 13:29:46 +0100

    day 10

---
+++ task_10/task.py
@@ -0,0 +1,88 @@
+import os
+from collections import defaultdict
+from itertools import combinations, permutations
+
+__here__ = os.path.dirname(__file__)
+
+TEST_DATA = '''\
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+'''.strip()
+with open(f'{__here__}/input.txt') as fp:
+    INPUT = fp.read().strip()
+
+def parse_data(data: str):
+    board = {}
+    trailheads = []
+    for y, line in enumerate(data.splitlines()):
+        for x, cell in enumerate(line):
+            if cell == '.':
+                continue
+            c = int(cell)
+            if c == 0:
+                trailheads.append((x, y))
+            board[(x, y)] = c
+    return board, trailheads
+
+def find_trails(board, head, allow_paths=False):
+    def neighbors(x, y):
+        cells = [
+            (x + 1, y),
+            (x - 1, y),
+            (x, y + 1),
+            (x, y - 1),
+        ]
+        for c in cells:
+            if c in board and board[c] == board[(x, y)] + 1:
+                yield c
+
+    def trails(x, y, path, allow_paths=False):
+        cells = list(neighbors(x, y))
+        current = board[(x, y)]
+        path.append((x, y))
+
+        if current == 9:
+            yield path
+        
+        if not cells:
+            yield None
+
+        for cell in cells:
+            if not allow_paths:
+                if cell in path:
+                    continue
+            yield from trails(cell[0], cell[1], path, allow_paths)
+
+    x, y = head
+    yield from trails(x, y, [], allow_paths)
+
+
+def trail_count(data: str):
+    board, trailheads = parse_data(data)
+
+    sum_a = 0
+    sum_b = 0
+
+    for head in trailheads:
+        distinct_trails = {tuple(t) for t in find_trails(board, head) if t}
+        sum_a += len(distinct_trails)
+        distinct_paths = {tuple(t) for t in find_trails(board, head, True) if t}
+        sum_b += len(distinct_paths)
+
+    yield sum_a
+    yield sum_b
+
+if __name__=='__main__':
+    test = trail_count(TEST_DATA)
+    assert next(test) == 36
+    assert next(test) == 81
+
+    run = trail_count(INPUT)
+    print(next(run))
+    print(next(run))
